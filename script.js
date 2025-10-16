f805aae72fa8032f329e5b5be836f625bd93638b
Dipanshu sharma <dipanshusharma8295@gmail.com>
2025-10-16 15:50:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+function page1Animation(){
+    
+var tl = gsap.timeline()
+
+
+tl.from("nav h1, nav h4, nav button",{
+     y:-30,
+    opacity:0,
+    delay:1,
+    duration:0.7,
+    stagger:0.15
+})
+
+tl.from(".center-part1 h1",{
+    x:-200,
+    opacity:0,
+    duration:0.5
+})
+
+tl.from(".center-part1 p",{
+    x:-100,
+    opacity:0,
+    duration:0.4
+})
+
+tl.from(".center-part1 button",{
+    
+    opacity:0,
+    duration:0.4
+})
+tl.from(".center-part2 img",{
+    
+    opacity:0,
+    duration:0.5
+},"-=0.5")
+
+tl.from(".section1bottom img",{
+    opacity:0,
+    y:30,
+    stagger:0.15,
+    duration:0.6
+})
+}
+
+
+page1Animation()
+
+gsap.from(".services h3",{
+    x:-100,
+    opacity:0,
+    duration:0.5,
+    scrollTrigger:{
+        trigger:".services h3",
+        scroller:"body",
+        markers:true,
+        start:"top 50%"
+    }
+})
